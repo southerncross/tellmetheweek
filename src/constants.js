db1f01a4820437a0f145095b2cd03de07afc674e
southerncross <lishunyang@mokahr.com>
2018-07-20 19:17:58 +0800

    update oncaller list

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const DAY_MS = 24 * 60 * 60 * 1000
 export const WEEK_MS = 7 * 24 * 60 * 60 * 1000
-export const ONCALLERS = ['李舜阳', '王思航', '郭麟', '范长勇', '钟嘉蕾', '张煜轩']
-export const ONCALLER_OFFET = 1 // 有时候oncaller名单发生变化，为保证还能对应上，这里需要用到offet
+export const ONCALLERS = ['李舜阳', '王思航', '郭麟', '钟嘉蕾', '张煜轩', '龙轩', '子森']
+export const ONCALLER_OFFET = 2 // 有时候oncaller名单发生变化，为保证还能对应上，这里需要用到offet
